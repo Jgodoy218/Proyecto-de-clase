a76e98c4891feeb28e9f462ca34903af3c12c7e4
CSU22-AINF <CSU22@academico.unab.ad>
2026-09-10 12:14:47 -0500

    Factory Products

--- database/migrations/2026_09_10_170943_add_category_id_to_table_products_table.php
+++ database/migrations/2026_09_10_170943_add_category_id_to_table_products_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('table_products', function (Blueprint $table) {
-            //
+            $table->foreignId('category_id')
+                ->nullable()
+                ->after('price')
+                ->constrained('categories')
+                ->onDelete('cascade');
         });
     }
 
@@ -22,7 +26,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('table_products', function (Blueprint $table) {
-            //
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
         });
     }
 };
