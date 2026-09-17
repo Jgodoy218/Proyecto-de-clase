8d2822b576d9f2d9054023b28d470fb90160ddb8
Julian Godoy <jgodoy218@unab.edu.co>
2026-09-17 11:22:19 -0500

    feat: agregar rutas del CRUD de productos

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,32 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
-Route::prefix('product')->controller(ProductController::class)->name('product.')->group(function () {
-    Route::get('/', 'index')->name('index');            // muestre listado de produtos          -> /product
-    Route::get('/create', 'create')->name('create');    // formulario para crear un producto     -> /product/create
-    Route::get('/{idProduct}', 'show')->name('show');   // detalle de un producto                -> /product/{idProduct}
-});
 
-//Todo apuntando al mismo Css ubicado en la carpeta public y se llama style.css
+Route::prefix('product')
+    ->controller(ProductController::class)
+    ->name('product.')
+    ->group(function () {
+
+        // Listar productos
+        Route::get('/', 'index')->name('index');
+
+        // Formulario para crear
+        Route::get('/create', 'create')->name('create');
+
+        // Guardar nuevo producto
+        Route::post('/', 'store')->name('store');
+
+        // Formulario para editar
+        Route::get('/{idProduct}/edit', 'edit')->name('edit');
+
+        // Actualizar producto
+        Route::put('/{idProduct}', 'update')->name('update');
+
+        // Eliminar producto
+        Route::delete('/{idProduct}', 'destroy')->name('destroy');
+
+        // Ver detalle
+        Route::get('/{idProduct}', 'show')->name('show');
+    });
+
+// Todo apuntando al mismo CSS ubicado en la carpeta public y se llama style.css
